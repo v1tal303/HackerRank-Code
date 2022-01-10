031eac30fbd1249e1547f192030ffdb677c81b13
VitaL <vitaldukm@gmail.com>
2022-01-10 19:31:39 +0000

    Create Print Function.py

---
+++ Python-Code/Introduction/Print Function.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+
+    
+string = "" 
+for i in range(1, n+1):
+    string += str(i)
+    
+print(string)
